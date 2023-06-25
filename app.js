dbc2f81e752ab050cd81fb5c7d61269a04e894a4
ShivamSaini26 <shivam.saini2126@gmail.com>
2023-06-25 11:06:47 +0530

    "nothing"

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 // imports
 const express = require("express");
-const path=require('path');
+const path = require('path');
 const bodyParser = require("body-parser");
 const app = express();
 
 //setting templating engine
-app.set('view engine','pug');
-app.set('views','views');
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 //routing paths
-const adminRoutes=require('./routes/admin.js');
-const shopRoutes=require('./routes/shop.js');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
 //serving files statically
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //parsing body
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //routing
-app.use('/admin',adminRoutes);//order matters when routing
+app.use('/admin', adminRoutes);//order matters when routing
 app.use(shopRoutes);//homepage of the app
 
-app.use((req,res, next)=>{ //404 error page
-    res.status(404).render('404',{pageTitle:'Page not Found !'})
+app.use((req, res, next) => { //404 error page
+    res.status(404).render('404', { pageTitle: 'Page not Found !' })
 });
 
-<<<<<<< HEAD
-//listening server
-=======
-//listening the server
->>>>>>> 44fcd07bdecdf440c1cbacfbe7c79bbdcbdc677c
+//server is listening
 app.listen(3000);
